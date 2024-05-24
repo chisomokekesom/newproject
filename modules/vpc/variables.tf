a45dc3a7fe990f5fda762ff975e34fdb478384c9
chisomokekesom <chisomokekesomboy@gmail.com>
2024-05-23 20:38:06 -0400

    added vpc

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+#Environment variables
+
+variable "region" {}
+
+variable "project_name" {}
+
+variable "environment" {}
+
+#vpc variables
+
+variable "vpc_cidr" {}
+
+variable "public-subnet-1-cidr" {}
+
+variable "public-subnet-2-cidr" {}
+
+variable "private-app-subnet-1-cidr" {}
+
+variable "private-app-subnet-2-cidr" {}
+
+variable "private-data-subnet-1-cidr" {}
+
+variable "private-data-subnet-2-cidr" {}
+
